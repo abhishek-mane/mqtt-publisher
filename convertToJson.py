dc45a8015820a30ada2da34d196071a7c14ec170
Abhishek Mane <abhishekmane@outlook.in>
2021-01-24 16:56:07 +0530

    updates

--- convertToJson.py
+++ convertToJson.py
@@ -12,7 +12,7 @@ with open('/Users/abhishekmane/Downloads/YWJhbG9uZQ==') as f:
             'sex': int(attrs[1].split(':')[1]),
             'length': float(attrs[2].split(':')[1]),
             'diameter': float(attrs[3].split(':')[1]),
-            'Height': float(attrs[4].split(':')[1]),
+            'height': float(attrs[4].split(':')[1]),
             'whole.weight': float(attrs[5].split(':')[1]),
             'shucked.weight': float(attrs[6].split(':')[1]),
             'viscera.weight': float(attrs[7].split(':')[1]),
