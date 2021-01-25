e59340dff03357f0d1d72bad66a15c05e89d796b
Abhishek Mane <abhishekmane@outlook.in>
2021-01-25 15:30:24 +0530

    fallback

--- creds.js
+++ creds.js
@@ -3,5 +3,5 @@
 module.exports = {
     accessKeyId: process.env['AWS_ACCESS_KEY_ID'],
     secretAccessKey: process.env['AWS_SECRET_ACCESS_KEY'],
-    region: process.env['AWS_DEFAULT_REGION']
+    region: process.env['AWS_DEFAULT_REGION'] || "ap-south-1"
 }
